34e2a418dc331574c7a2a240474e1e3a733f0a00
amanyadav902879 <kaladevil902879@gmail.com>
2025-04-15 15:22:44 +0530

    hi

--- shop.js
+++ shop.js
@@ -18,3 +18,20 @@
             btn.classList.add("active");
         });
     });
+
+    const scrollBtn = document.getElementById('scrollUpBtn');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    scrollBtn.classList.add('show');
+  } else {
+    scrollBtn.classList.remove('show');
+  }
+});
+
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
